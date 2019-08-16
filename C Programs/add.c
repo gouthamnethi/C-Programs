8bd1e0c451e41db00c3a27574665bdc3da601431
gouthamnethi <32320474+gouthamnethi@users.noreply.github.com>
2019-08-16 19:28:16 +0530

    Update add.c

--- C Programs/add.c
+++ C Programs/add.c
@@ -1,12 +1,13 @@
-#include<stdio.h>
-int main()
+//Sum of the two numbers.
+#include<stdio.h> //Header files
+int main() // Main Program
 {
-	int a,b,sum;
-	printf("Enter the value of a:");
-	scanf("%d",&a);
-	printf("Enter the value of b:");
-	scanf("%d",&b);
-	sum=a+b;
-	printf("The sum of the %d and %d is %d",a,b,sum);
+	int a,b,sum; //Declaring the variables
+	printf("Enter the value of a:"); //Printing the matter on the screen which is in quoted 
+	scanf("%d",&a); //Reading the value of a from user
+	printf("Enter the value of b:"); //Printing the matter entered in the double quotes
+	scanf("%d",&b); //Reading the input from user
+	sum=a+b; //Adding the value of a and b
+	printf("The sum of the %d and %d is %d",a,b,sum); //printing the value of a and b along with the sum on output screen
 	return 0;
-}
+} //End of the program
